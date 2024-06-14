36374a31eee178e71833fdaef02fdcc6f9305896
dana-akesh <86303193+dana-akesh@users.noreply.github.com>
2024-06-14 16:59:37 +0300

    intial commit

---
+++ hotel-management-system/src/test/java/com/bzu/hotel_management_system/HotelManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bzu.hotel_management_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
