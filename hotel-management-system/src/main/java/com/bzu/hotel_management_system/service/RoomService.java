636e61b2be2d0ad898b397a3f0e1ce5dd894854a
dana-akesh <86303193+dana-akesh@users.noreply.github.com>
2024-06-16 21:00:45 +0300

    impleneted the room controller with the methods used in the service

--- hotel-management-system/src/main/java/com/bzu/hotel_management_system/service/RoomService.java
+++ hotel-management-system/src/main/java/com/bzu/hotel_management_system/service/RoomService.java
@@ -1,8 +1,15 @@
 package com.bzu.hotel_management_system.service;
 
 import com.bzu.hotel_management_system.DTO.RoomDTO;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
 public interface RoomService {
+    // get all rooms
+    List<RoomDTO> getAllRooms();
+
     RoomDTO getRoomById(Long id);
 
     RoomDTO addRoom(RoomDTO roomDTO);
