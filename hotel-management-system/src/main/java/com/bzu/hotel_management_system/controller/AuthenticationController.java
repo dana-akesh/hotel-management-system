a0645ba081e36b2dc1de466c1b3e860476fcbd74
dana-akesh <86303193+dana-akesh@users.noreply.github.com>
2024-06-18 14:24:05 +0300

    added swagger opertaions to authentication controller

--- hotel-management-system/src/main/java/com/bzu/hotel_management_system/controller/AuthenticationController.java
+++ hotel-management-system/src/main/java/com/bzu/hotel_management_system/controller/AuthenticationController.java
@@ -4,6 +4,10 @@ import com.bzu.hotel_management_system.request.AuthenticationRequest;
 import com.bzu.hotel_management_system.request.RegisterRequest;
 import com.bzu.hotel_management_system.response.AuthenticationResponse;
 import com.bzu.hotel_management_system.service.AuthenticationService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.http.ResponseEntity;
@@ -24,12 +28,77 @@ public class AuthenticationController {
         this.authenticationService = authenticationService;
     }
 
+    // register
+    @Operation(
+            summary = "Register",
+            description = "Register a new user",
+            responses = {
+                    @ApiResponse(
+                            description = "Successful operation",
+                            responseCode = "200",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = AuthenticationResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            description = "Bad request, invalid json",
+                            responseCode = "400",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = AuthenticationResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            description = "Unauthorized access",
+                            responseCode = "401",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = AuthenticationResponse.class)
+                            )
+                    ),
+            }
+    )
+
     @PostMapping("/register")
     public ResponseEntity<AuthenticationResponse> register(
             @RequestBody RegisterRequest request
     ) {
         return ResponseEntity.ok(authenticationService.register(request));
     }
+
+    // authenticate
+    @Operation(
+            summary = "Authenticate",
+            description = "Authenticate a user",
+            responses = {
+                    @ApiResponse(
+                            description = "Successful operation",
+                            responseCode = "200",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = AuthenticationResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            description = "Bad request, invalid json",
+                            responseCode = "400",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = AuthenticationResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            description = "Unauthorized access",
+                            responseCode = "401",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = AuthenticationResponse.class)
+                            )
+                    ),
+            }
+    )
+
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate(
             @RequestBody AuthenticationRequest request
@@ -37,6 +106,38 @@ public class AuthenticationController {
         return ResponseEntity.ok(authenticationService.authenticate(request));
     }
 
+
+    // refresh token
+    @Operation(
+            summary = "Refresh token",
+            description = "Refresh",
+            responses = {
+                    @ApiResponse(
+                            description = "Successful operation",
+                            responseCode = "200",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = AuthenticationResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            description = "Bad request, invalid json",
+                            responseCode = "400",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = AuthenticationResponse.class)
+                            )
+                    ),
+                    @ApiResponse(
+                            description = "Unauthorized access",
+                            responseCode = "401",
+                            content = @Content(
+                                    mediaType = "application/json",
+                                    schema = @Schema(implementation = AuthenticationResponse.class)
+                            )
+                    ),
+            }
+    )
     @PostMapping("/refresh-token")
     public void refreshToken(
             HttpServletRequest request,
