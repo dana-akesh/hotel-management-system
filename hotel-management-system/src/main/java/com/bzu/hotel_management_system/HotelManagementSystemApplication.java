23c77821f1733ddca4f63a2bdff7f07d07d494ac
dana-akesh <86303193+dana-akesh@users.noreply.github.com>
2024-06-18 23:35:05 +0300

    trying to add dynamic data insertion

--- hotel-management-system/src/main/java/com/bzu/hotel_management_system/HotelManagementSystemApplication.java
+++ hotel-management-system/src/main/java/com/bzu/hotel_management_system/HotelManagementSystemApplication.java
@@ -1,7 +1,9 @@
 package com.bzu.hotel_management_system;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class HotelManagementSystemApplication {
@@ -10,4 +12,8 @@ public class HotelManagementSystemApplication {
 		SpringApplication.run(HotelManagementSystemApplication.class, args);
 	}
 
+	/*@Bean
+	CommandLineRunner runner(){
+
+	}*/
 }
